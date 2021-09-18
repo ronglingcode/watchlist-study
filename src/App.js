e73a16b48b2bf40ab901c332c9b2d193bd0a1184
rongvrv <77297902+rongvrv@users.noreply.github.com>
2021-09-18 12:02:11 -0700

    add data grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+   const rowData = [
+       {symbol: "MSFT", model: "Celica", price: 35000},
+       {symbol: "AAPL", model: "Mondeo", price: 32000},
+       {symbol: "TSLA", model: "Boxter", price: 72000}
+   ];
+
+   return (
+       <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+           <AgGridReact
+               rowData={rowData}
+               defaultColDef={{
+                flex: 1,
+                minWidth: 110,
+                editable: true,
+                resizable: true,
+              }}>
+               <AgGridColumn field="symbol"></AgGridColumn>
+               <AgGridColumn field="model"></AgGridColumn>
+               <AgGridColumn field="price"></AgGridColumn>
+           </AgGridReact>
+       </div>
+   );
+};
 
 export default App;
