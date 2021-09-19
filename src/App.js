1ce8257c6cf77563754d14bd4f733654f2ef52ab
rongvrv <77297902+rongvrv@users.noreply.github.com>
2021-09-19 15:14:23 -0700

    add plan column

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const App = () => {
     let rowData = [
-       {symbol: "MSFT", gap: "Gap Up", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "AAPL", gap: "Gap Down",lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Low volume'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
-       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading'},
+       {symbol: "MSFT", gap: "Gap Up", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "AAPL", gap: "Gap Down",lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Low volume', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
+       {symbol: "TSLA", gap: "Flat", lastFiveDays: "持续上涨", lastThreeWeeks: "持续上涨", volume: 'Active trading', plan: ''},
     ];
 
-    const print = () => {
+    const planTheTrade = () => {
         console.log('button clicked');
         gridApi.forEachNode((rowNode, index) => {
             console.log(rowNode.data);
+            rowNode.setDataValue('plan', 'test')
         });
     }
     const [gridApi, setGridApi] = useState(null);
@@ -34,42 +35,45 @@ const App = () => {
       };
 
     return (
-        <div className="ag-theme-alpine" style={{height: 600, width: 1000}}>
-            <AgGridReact
-                onGridReady={onGridReady}
-                rowData={rowData}
-                defaultColDef={{
-                    flex: 1,
-                    minWidth: 110,
-                    editable: true,
-                    resizable: true,
-                }}>
-                <AgGridColumn field="symbol"></AgGridColumn>
-                <AgGridColumn 
-                    field="gap"
-                    cellEditor="agRichSelectCellEditor"
-                    cellEditorParams={{ cellHeight: 35, values: ['Gap Up', 'Gap Down', 'Flat'] }}
-                />
-                <AgGridColumn 
-                    headerName="过去5天走势"
-                    field="lastFiveDays"
-                    cellEditor="agRichSelectCellEditor"
-                    cellEditorParams={{ cellHeight: 35, values: ['持续上涨', '持续下跌', '盘整'] }}
-                />
-                <AgGridColumn 
-                    headerName="过去3周走势"
-                    field="lastThreeWeeks"
-                    cellEditor="agRichSelectCellEditor"
-                    cellEditorParams={{ cellHeight: 35, values: ['持续上涨', '持续下跌', '盘整'] }}
-                />
-                <AgGridColumn 
-                    headerName="盘前交易"
-                    field="volume"
-                    cellEditor="agRichSelectCellEditor"
-                    cellEditorParams={{ cellHeight: 35, values: ['Low volume', 'Active trading'] }}
-                />
-            </AgGridReact>
-            <button onClick={print}>Test</button>
+        <div>
+            <div className="ag-theme-alpine" style={{height: 600, width: 1000}}>
+                <AgGridReact
+                    onGridReady={onGridReady}
+                    rowData={rowData}
+                    defaultColDef={{
+                        flex: 1,
+                        minWidth: 110,
+                        editable: true,
+                        resizable: true,
+                    }}>
+                    <AgGridColumn field="symbol"></AgGridColumn>
+                    <AgGridColumn 
+                        field="gap"
+                        cellEditor="agRichSelectCellEditor"
+                        cellEditorParams={{ cellHeight: 35, values: ['Gap Up', 'Gap Down', 'Flat'] }}
+                    />
+                    <AgGridColumn 
+                        headerName="过去5天走势"
+                        field="lastFiveDays"
+                        cellEditor="agRichSelectCellEditor"
+                        cellEditorParams={{ cellHeight: 35, values: ['持续上涨', '持续下跌', '盘整'] }}
+                    />
+                    <AgGridColumn 
+                        headerName="过去3周走势"
+                        field="lastThreeWeeks"
+                        cellEditor="agRichSelectCellEditor"
+                        cellEditorParams={{ cellHeight: 35, values: ['持续上涨', '持续下跌', '盘整'] }}
+                    />
+                    <AgGridColumn 
+                        headerName="盘前交易"
+                        field="volume"
+                        cellEditor="agRichSelectCellEditor"
+                        cellEditorParams={{ cellHeight: 35, values: ['Low volume', 'Active trading'] }}
+                    />
+                    <AgGridColumn field="plan"/>
+                </AgGridReact>
+            </div>
+            <button onClick={planTheTrade}>Plan the trade</button>
         </div>
    );
 };
